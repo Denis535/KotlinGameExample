a52cc11cbe6bb77cdffc3d40c716aef99500c157
Denis535 <dddenisss35@gmail.com>
2025-11-25 20:25:10 +0200

    Clear

--- 00.main/build.gradle.kts
+++ 00.main/build.gradle.kts
@@ -1,16 +1,12 @@
 plugins {
     this.id("org.jetbrains.kotlin.jvm") version "2.2.21"
-    this.id("maven-publish")
+//    this.id("maven-publish")
 }
 
-group = project.group
-version = project.version
-description = project.description
-
 kotlin {
-    this.jvmToolchain(21)
+    this.jvmToolchain(24)
     this.compilerOptions {
-        this.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+        this.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24
     }
 }
 
@@ -24,40 +20,7 @@ dependencies {
     this.testImplementation(this.kotlin("test"))
 }
 
-publishing {
-    this.publications {
-        this.create<MavenPublication>("mavenJava") {
-            this.from(components["java"])
-            this.groupId = project.group.toString()
-            this.artifactId = rootProject.name + '-' + project.name
-            this.version = project.version.toString()
-            this.pom {
-                this.name = project.name
-                this.description = project.description
-                this.url = project.findProperty("url").toString()
-                this.licenses {
-                    this.license {
-                        this.name = "MIT License"
-                        this.url = "https://opensource.org/licenses/MIT"
-                    }
-                }
-                this.developers {
-                    this.developer {
-                        this.id = "denis535"
-                        this.name = "Denis535"
-                    }
-                }
-                this.scm {
-                    this.connection = "scm:git:git://github.com/Denis535/kotlin-game-example.git"
-                    this.developerConnection = "scm:git:ssh://git@github.com:Denis535/kotlin-game-example.git"
-                    this.url = "https://github.com/Denis535/kotlin-game-example"
-                }
-            }
-        }
-    }
-}
-
 tasks.register("run", JavaExec::class) {
     this.classpath = sourceSets["main"].runtimeClasspath
-    this.mainClass.set("com.denis535.kotlin_game_example.ProgramKt")
+    this.mainClass = "com.denis535.kotlin_game_example.ProgramKt"
 }
