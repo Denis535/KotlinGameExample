0ddafcdcaf27fba413439fc3b7f2040953473191
Denis535 <dddenisss35@gmail.com>
2025-11-27 15:14:41 +0200

    lwjgl

--- 10.common/build.gradle.kts
+++ 10.common/build.gradle.kts
@@ -11,15 +11,13 @@ kotlin {
 }
 
 dependencies {
-    this.api(this.platform("org.lwjgl:lwjgl-bom:3.3.6"))
-    this.api("org.lwjgl", "lwjgl")
-    this.api("org.lwjgl", "lwjgl-glfw")
-    this.api("org.lwjgl", "lwjgl-nuklear")
-    this.api("org.lwjgl", "lwjgl", classifier = "natives-windows")
-    this.api("org.lwjgl", "lwjgl-glfw", classifier = "natives-windows")
-    this.api("org.lwjgl", "lwjgl-nuklear", classifier = "natives-windows")
     this.api("io.github.denis535:game-framework-pro:1.0.0")
     this.api("io.github.denis535:game-framework-pro-extensions:1.0.0")
+    this.implementation(this.platform("org.lwjgl:lwjgl-bom:3.3.6"))
+    this.implementation("org.lwjgl", "lwjgl")
+    this.implementation("org.lwjgl", "lwjgl-glfw")
+    this.implementation("org.lwjgl", "lwjgl", classifier = "natives-windows")
+    this.implementation("org.lwjgl", "lwjgl-glfw", classifier = "natives-windows")
     this.testImplementation(this.kotlin("test"))
 }
 
