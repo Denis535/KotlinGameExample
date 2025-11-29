96e2215d91a6f7e016221bdf6f039ab1d937f0eb
Denis535 <dddenisss35@gmail.com>
2025-11-29 20:02:39 +0200

    GLFW

--- 10.common/src/main/kotlin/com/denis535/kotlin_game_example/Engine.kt
+++ 10.common/src/main/kotlin/com/denis535/kotlin_game_example/Engine.kt
@@ -6,6 +6,52 @@ public object Engine {
 
     private var Window: Long = 0
 
+    public val IsWindowShown: Boolean
+        get() {
+            return this.Window != 0L
+        }
+
+    public val IsWindowFullscreen: Boolean
+        get() {
+            check(this.Window != 0L)
+            val monitor = GLFW.glfwGetWindowMonitor(this.Window).also { GLFW2.ThrowErrorIfNeeded() }
+            return monitor != 0L
+        }
+
+    public val WindowPosition: Pair<Int, Int>
+        get() {
+            check(this.Window != 0L)
+            val posX = IntArray(1)
+            val posY = IntArray(1)
+            GLFW.glfwGetWindowPos(this.Window, posX, posY)
+            return posX[0] to posY[0]
+        }
+
+    public val WindowSize: Pair<Int, Int>
+        get() {
+            check(this.Window != 0L)
+            val width = IntArray(1)
+            val height = IntArray(1)
+            GLFW.glfwGetWindowSize(this.Window, width, height)
+            return width[0] to height[0]
+        }
+
+    public val Time: Double
+        get() {
+            check(this.Window != 0L)
+            return GLFW.glfwGetTime().also { GLFW2.ThrowErrorIfNeeded() }
+        }
+
+    public var NumberOfFrame: Int = 0
+        get() {
+            check(this.Window != 0L)
+            return field
+        }
+        private set(value) {
+            check(this.Window != 0L)
+            field = value
+        }
+
     public fun Initialize() {
         check(this.Window == 0L)
         GLFW.glfwInit().also { GLFW2.ThrowErrorIfNeeded() }
@@ -49,13 +95,31 @@ public object Engine {
         GLFW.glfwSetWindowShouldClose(this.Window, true)
     }
 
+    public fun SetIsWindowFullscreen(isFullscreen: Boolean) {
+        check(this.Window != 0L)
+        val monitor = GLFW.glfwGetPrimaryMonitor().also { GLFW2.ThrowErrorIfNeeded() }
+        val videoMode = GLFW.glfwGetVideoMode(monitor)!!.also { GLFW2.ThrowErrorIfNeeded() }
+        if (isFullscreen) {
+            GLFW.glfwSetWindowMonitor(this.Window, monitor, 0, 0, videoMode.width(), videoMode.height(), videoMode.refreshRate()).also { GLFW2.ThrowErrorIfNeeded() }
+        } else {
+            GLFW.glfwSetWindowMonitor(this.Window, 0, (videoMode.width() - 1280) / 2, (videoMode.height() - 720) / 2, 1280, 720, 0).also { GLFW2.ThrowErrorIfNeeded() }
+        }
+    }
+
     public fun Run() {
         check(this.Window != 0L)
+        this.NumberOfFrame = 0
         while (!GLFW.glfwWindowShouldClose(this.Window)) {
             GLFW.glfwPollEvents().also { GLFW2.ThrowErrorIfNeeded() }
+            if (GLFW.glfwGetKey(this.Window, GLFW.GLFW_KEY_LEFT_ALT) == GLFW.GLFW_PRESS || GLFW.glfwGetKey(this.Window, GLFW.GLFW_KEY_RIGHT_ALT) == GLFW.GLFW_PRESS) {
+                if (GLFW.glfwGetKey(this.Window, GLFW.GLFW_KEY_ENTER) == GLFW.GLFW_PRESS) {
+                    this.SetIsWindowFullscreen(true)
+                }
+            }
             // Update()
             // Draw()
             GLFW.glfwSwapBuffers(this.Window).also { GLFW2.ThrowErrorIfNeeded() }
+            this.NumberOfFrame++
         }
         GLFW.glfwDestroyWindow(this.Window).also { GLFW2.ThrowErrorIfNeeded() }
         this.Window = 0L
