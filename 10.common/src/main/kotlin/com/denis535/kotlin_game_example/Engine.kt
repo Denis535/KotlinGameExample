7a24cfde774533203dbfe91429b42af1219dd1ed
Denis535 <dddenisss35@gmail.com>
2025-11-30 23:07:08 +0200

    Engine

--- 10.common/src/main/kotlin/com/denis535/kotlin_game_example/Engine.kt
+++ 10.common/src/main/kotlin/com/denis535/kotlin_game_example/Engine.kt
@@ -22,7 +22,17 @@ public class Engine : AutoCloseable {
             field = value
         }
 
-    public var Time: Double = 0.0
+    public var FixedTick: Int = 0
+        get() {
+            check(this.IsRunning)
+            return field
+        }
+        private set(value) {
+            check(this.IsRunning)
+            field = value
+        }
+
+    public var Fps: Double = 0.0
         get() {
             check(this.IsRunning)
             return field
@@ -44,27 +54,21 @@ public class Engine : AutoCloseable {
     public fun Run() {
         this.IsRunning = true
         this.Tick = 0
-        this.Time = 0.0
-        var timeAccumulator = 0.0
+        this.FixedTick = 0
+        this.Fps = 0.0
+        var time = 0.0
         var deltaTime = 0.0
-        val fixedDeltaTime = 1.0 / 25.0
         while (!this.Window.IsClosingRequested) {
-            val startTime = this.Window.Time
-            run {
+            deltaTime = run {
+                val startTime = this.Window.Time
                 this.OnFrameBegin()
-                while (timeAccumulator >= fixedDeltaTime) {
-                    this.OnFixedUpdate(fixedDeltaTime)
-                    timeAccumulator -= fixedDeltaTime
-                }
-                this.OnUpdate(deltaTime)
-                this.OnDraw(deltaTime)
+                this.OnFrame(time, deltaTime)
                 this.OnFrameEnd()
+                val endTime = this.Window.Time
+                endTime - startTime
             }
-            val endTime = this.Window.Time
-            deltaTime = endTime - startTime
-            timeAccumulator += deltaTime
-            this.Tick++
-            this.Time += deltaTime
+            time += deltaTime
+            this.Fps = 1.0 / deltaTime
         }
         this.IsRunning = false
     }
@@ -73,11 +77,33 @@ public class Engine : AutoCloseable {
         GLFW.glfwPollEvents().also { GLFW2.ThrowErrorIfNeeded() }
     }
 
-    private fun OnFixedUpdate(deltaTime: Double) {
+    private fun OnFrame(time: Double, deltaTime: Double) {
+        if (this.FixedTick == 0) {
+            this.OnFixedUpdate(0.0)
+            this.FixedTick++
+        } else {
+            val fixedDeltaTime = 1.0 / 25.0
+            val desiredFixedTicks = (time / fixedDeltaTime).toInt()
+            while (this.FixedTick < desiredFixedTicks) {
+                this.OnFixedUpdate(fixedDeltaTime)
+                this.FixedTick++
+            }
+        }
+        this.OnUpdate(deltaTime)
+        this.OnDraw(deltaTime)
+        this.Tick++
+    }
 
+    private fun OnFrameEnd() {
+        GLFW.glfwSwapBuffers(this.Window.NativeWindowPointer).also { GLFW2.ThrowErrorIfNeeded() }
+    }
+
+    private fun OnFixedUpdate(deltaTime: Double) {
+//        println("OnFixedUpdate")
     }
 
     private fun OnUpdate(deltaTime: Double) {
+//        println("OnUpdate")
         if (GLFW.glfwGetKey(this.Window.NativeWindowPointer, GLFW.GLFW_KEY_LEFT_ALT) == GLFW.GLFW_PRESS || GLFW.glfwGetKey(this.Window.NativeWindowPointer, GLFW.GLFW_KEY_RIGHT_ALT) == GLFW.GLFW_PRESS) {
             if (GLFW.glfwGetKey(this.Window.NativeWindowPointer, GLFW.GLFW_KEY_ENTER) == GLFW.GLFW_PRESS) {
                 this.Window.IsFullscreen = !this.Window.IsFullscreen
@@ -87,10 +113,6 @@ public class Engine : AutoCloseable {
 
     private fun OnDraw(deltaTime: Double) {
 
-    }
-
-    private fun OnFrameEnd() {
-        GLFW.glfwSwapBuffers(this.Window.NativeWindowPointer).also { GLFW2.ThrowErrorIfNeeded() }
     }
 
 }
