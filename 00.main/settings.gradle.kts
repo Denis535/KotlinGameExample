fa68c79a0a0d91c6601150bdd148a50014d65cb8
Denis535 <dddenisss35@gmail.com>
2025-12-05 18:20:36 +0200

    Refactoring

--- 00.main/settings.gradle.kts
+++ 00.main/settings.gradle.kts
@@ -1,19 +1,1 @@
-pluginManagement {
-    this.repositories {
-        this.gradlePluginPortal()
-        this.mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    this.repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
-    this.repositories {
-        this.mavenCentral()
-    }
-}
-
-plugins {
-    this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 rootProject.name = "main"
