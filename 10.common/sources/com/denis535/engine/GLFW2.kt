0367f90743dbd41b446294aef66feab227d6776c
Denis535 <dddenisss35@gmail.com>
2025-12-10 16:35:40 +0200

    GLFW2

--- 10.common/sources/com/denis535/engine/GLFW2.kt
+++ 10.common/sources/com/denis535/engine/GLFW2.kt
@@ -1,29 +1,30 @@
 package com.denis535.engine
 
+import kotlinx.cinterop.*
 import glfw.*
 
 internal object GLFW2 {
-//
-//    private val PointerBuffer = MemoryUtil.memAllocPointer(1)
-//
-//    public fun ThrowErrorIfNeeded() {
-//        val (error, desc) = this.GetError()
-//        if (error != 0) {
-//            val message = if (desc != null) {
-//                "GLFW Error: $error, $desc"
-//            } else {
-//                "GLFW Error: $error"
-//            }
-//            error(message)
-//        }
-//    }
 
-//    private fun GetError(): Pair<Int, String?> {
-//        return memScoped {
-//            val descPtr = alloc<CPointerVar<ByteVar>>()
-//            val error = glfwGetError(descPtr.ptr)
-//            val description = descPtr.value?.toKString()  // если указатель != null → строка
-//            Pair(error, description)
-//        }
-//    }
+    public fun ThrowErrorIfNeeded() {
+        val (error, desc) = this.GetError()
+        if (error != 0) {
+            val message = if (desc != null) {
+                "GLFW Error: $error, $desc"
+            } else {
+                "GLFW Error: $error"
+            }
+            error(message)
+        }
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    private fun GetError(): Pair<Int, String?> {
+        return memScoped {
+            val descPtr = this.alloc<CPointerVar<ByteVar>>()
+            val error = glfwGetError(descPtr.ptr)
+            val description = descPtr.value?.toKString()  // если указатель != null → строка
+            Pair(error, description)
+        }
+    }
+
 }
