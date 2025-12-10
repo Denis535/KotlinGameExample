38073e0ea28697dbc31fb11688509d97c435a781
Denis535 <dddenisss35@gmail.com>
2025-12-10 23:15:56 +0200

    KotlinOpenFoundation

--- 10.common/build.gradle.kts
+++ 10.common/build.gradle.kts
@@ -3,18 +3,13 @@ plugins {
 }
 
 kotlin {
-    this.mingwX64 {
-        this.compilations["main"].cinterops {
-            val glfw by creating {
-                this.definitionFile = file("native/glfw.def")
-            }
-        }
-    }
+    this.mingwX64()
     this.sourceSets {
         val commonMain by this.getting {
             this.kotlin.srcDir("sources")
             this.resources.srcDir("resources")
             this.dependencies {
+                this.implementation("io.github.kotlinopenfoundation.glfw:glfw-kotlin:0.1.0-dev")
                 this.api("io.github.denis535:game-framework-pro:1.0.1")
                 this.api("io.github.denis535:game-framework-pro-extensions:1.0.1")
             }
