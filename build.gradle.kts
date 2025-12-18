e24483d669dfddb96963fc5888f6f6c112b287df
Denis <dddenisss35@gmail.com>
2025-12-18 14:42:11 +0200

    Linux

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,20 +14,32 @@ if (System.getProperty("os.name").lowercase().contains("windows")) {
     }
 }
 
-//tasks.register("publish") {
-//    this.dependsOn(tasks.named("clean-dist"), tasks.named("publish-to-dist"))
-//}
-//
-//tasks.register<Delete>("clean-dist") {
-//    this.delete(layout.projectDirectory.dir("dist"))
-//}
-//
-//tasks.register<Copy>("publish-to-dist") {
-//    val main = project(":main")
-//    this.dependsOn(main.tasks.named("linkReleaseExecutableMingwX64"))
-//    this.from(main.layout.buildDirectory.dir("bin/mingwX64/releaseExecutable")) {
-//        this.include("*.exe")
-//        this.rename { "Launcher.exe" }
-//    }
-//    this.into(layout.projectDirectory.dir("dist"))
-//}
+tasks.register("publish") {
+    this.dependsOn(tasks.named("clean-dist"), tasks.named("publish-to-dist"))
+}
+
+tasks.register<Delete>("clean-dist") {
+    this.delete(layout.projectDirectory.dir("dist"))
+}
+
+if (System.getProperty("os.name").lowercase().contains("windows")) {
+    tasks.register<Copy>("publish-to-dist") {
+        val main = project(":main")
+        this.dependsOn(main.tasks.named("linkReleaseExecutableMingwX64"))
+        this.from(main.layout.buildDirectory.dir("bin/mingwX64/releaseExecutable")) {
+            this.include("*.exe")
+            this.rename { "Launcher.exe" }
+        }
+        this.into(layout.projectDirectory.dir("dist"))
+    }
+} else if (System.getProperty("os.name").lowercase().contains("linux")) {
+    tasks.register<Copy>("publish-to-dist") {
+        val main = project(":main")
+        this.dependsOn(main.tasks.named("linkReleaseExecutableLinuxX64"))
+        this.from(main.layout.buildDirectory.dir("bin/linuxX64/releaseExecutable")) {
+            include("*.kexe")
+            rename { "Launcher" } // без .exe на Linux
+        }
+        this.into(layout.projectDirectory.dir("dist"))
+    }
+}
