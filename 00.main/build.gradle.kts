fa4af2b7fd943687b1cd906542412b1ed224850f
Denis <dddenisss35@gmail.com>
2025-12-25 00:48:27 +0200

    mingw

--- 00.main/build.gradle.kts
+++ 00.main/build.gradle.kts
@@ -3,15 +3,15 @@ plugins {
 }
 
 kotlin {
-//    this.mingwX64 {
-//        this.binaries {
-//            this.executable {
-//                this.baseName = rootProject.name
-//                this.entryPoint = "com.denis535.kotlin_game_example.Main"
-//                this.linkerOpts("-Wl,-subsystem,windows")
-//            }
-//        }
-//    }
+    this.mingwX64 {
+        this.binaries {
+            this.executable {
+                this.baseName = rootProject.name
+                this.entryPoint = "com.denis535.kotlin_game_example.Main"
+                this.linkerOpts("-Wl,-subsystem,windows")
+            }
+        }
+    }
     this.linuxX64 {
         this.binaries {
             this.executable {
@@ -32,7 +32,43 @@ kotlin {
                 this.implementation(this.project(":common"))
             }
         }
-//        val mingwX64Main by getting {}
+        val mingwX64Main by getting {}
         val linuxX64Main by getting {}
     }
+}
+
+tasks.register<Copy>("copy-mingw-debug-dependencies") {
+    this.from("SDL-release-3.2.28/install/x86_64-w64-mingw32/bin/SDL3.dll")
+    this.into(kotlin.mingwX64().binaries.getExecutable("DEBUG").outputDirectory)
+}
+
+tasks.register<Copy>("copy-mingw-release-dependencies") {
+    this.from("SDL-release-3.2.28/install/x86_64-w64-mingw32/bin/SDL3.dll")
+    this.into(kotlin.mingwX64().binaries.getExecutable("RELEASE").outputDirectory)
+}
+
+tasks.register<Copy>("copy-linux-debug-dependencies") {
+    this.from("SDL-release-3.2.28/install/x86_64-linux-gnu/lib/libSDL3.so")
+    this.into(kotlin.linuxX64().binaries.getExecutable("DEBUG").outputDirectory)
+}
+
+tasks.register<Copy>("copy-linux-release-dependencies") {
+    this.from("SDL-release-3.2.28/install/x86_64-linux-gnu/lib/libSDL3.so")
+    this.into(kotlin.linuxX64().binaries.getExecutable("RELEASE").outputDirectory)
+}
+
+tasks.named("linkDebugExecutableMingwX64") {
+    this.finalizedBy("copy-mingw-debug-dependencies")
+}
+
+tasks.named("linkReleaseExecutableMingwX64") {
+    this.finalizedBy("copy-mingw-release-dependencies")
+}
+
+tasks.named("linkDebugExecutableLinuxX64") {
+    this.finalizedBy("copy-linux-debug-dependencies")
+}
+
+tasks.named("linkReleaseExecutableLinuxX64") {
+    this.finalizedBy("copy-linux-release-dependencies")
 }
--- 01.ui/build.gradle.kts
+++ 01.ui/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 kotlin {
-//    this.mingwX64()
+    this.mingwX64()
     this.linuxX64()
     this.sourceSets {
         val commonMain by this.getting {
@@ -15,7 +15,7 @@ kotlin {
                 this.implementation(this.project(":common"))
             }
         }
-//        val mingwX64Main by getting {}
+        val mingwX64Main by getting {}
         val linuxX64Main by getting {}
     }
 }
