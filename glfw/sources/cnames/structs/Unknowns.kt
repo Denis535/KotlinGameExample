03f258e445474826f251ac552057b844daff5eca
Denis <dddenisss35@gmail.com>
2026-01-01 01:18:00 +0200

    Removed glfw

--- glfw/sources/cnames/structs/Unknowns.kt
+++
@@ -1,9 +0,0 @@
-@file:OptIn(ExperimentalForeignApi::class)
-
-package cnames.structs
-
-import kotlinx.cinterop.*
-
-typealias GLFWmonitor = COpaque
-typealias GLFWwindow = COpaque
-typealias GLFWcursor = COpaque
