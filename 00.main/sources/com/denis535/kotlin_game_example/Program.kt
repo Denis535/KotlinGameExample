5b48185cb5e87425cc482336b6dc737726169512
Denis <dddenisss35@gmail.com>
2025-12-31 13:52:12 +0200

    Engine2

--- 00.main/sources/com/denis535/kotlin_game_example/Program.kt
+++ 00.main/sources/com/denis535/kotlin_game_example/Program.kt
@@ -29,130 +29,6 @@ public fun Main(args: Array<String>) {
 }
 
 public class Program : AbstractProgram2<Theme, Screen, Router, Application> {
-    private class Engine2 : Engine {
-
-        public constructor() : super("Kotlin Game Example")
-
-        public override fun close() {
-            super.close()
-        }
-
-        protected override fun OnStart(info: FrameInfo) {
-        }
-
-        protected override fun OnStop(info: FrameInfo) {
-        }
-
-        protected override fun OnFixedUpdate(info: FrameInfo) {
-        }
-
-        protected override fun OnUpdate(info: FrameInfo) {
-        }
-
-    }
-
-    private class MainWindow2 : MainWindow {
-
-        public constructor() : super(MainWindowDesc.Window("Kotlin Game Example"))
-
-        public override fun close() {
-            super.close()
-        }
-
-        protected override fun OnShow() {
-
-        }
-
-        protected override fun OnHide() {
-
-        }
-
-        protected override fun OnDraw(info: FrameInfo) {
-
-        }
-
-        protected override fun OnMouseCursorMove(event: MouseCursorMoveEvent) {
-        }
-
-        protected override fun OnMouseButtonPress(event: MouseButtonActionEvent) {
-        }
-
-        protected override fun OnMouseButtonRelease(event: MouseButtonActionEvent) {
-        }
-
-        protected override fun OnMouseWheelScroll(event: MouseWheelScrollEvent) {
-        }
-
-        protected override fun OnKeyPress(event: KeyActionEvent) {
-            if (event.Key == Key.F1) {
-                this.IsFullScreen = !this.IsFullScreen
-            }
-
-            if (event.Key == Key.F2) {
-                this.IsResizable = !this.IsResizable
-            }
-
-            if (event.Key == Key.F3) {
-                this.Cursor.IsVisible = !this.Cursor.IsVisible
-            }
-            if (event.Key == Key.F4) {
-                this.Cursor.IsGrabbed = !this.Cursor.IsGrabbed
-            }
-            if (event.Key == Key.F5) {
-                this.Cursor.IsCaptured = !this.Cursor.IsCaptured
-            }
-            if (event.Key == Key.F6) {
-                this.Cursor.IsLocked = !this.Cursor.IsLocked
-            }
-
-            if (event.Key == Key.Digit_1) {
-                this.Cursor.Style = CursorStyle.Arrow
-            }
-            if (event.Key == Key.Digit_2) {
-                this.Cursor.Style = CursorStyle.Text
-            }
-            if (event.Key == Key.Digit_3) {
-                this.Cursor.Style = CursorStyle.Pointer
-            }
-            if (event.Key == Key.Digit_4) {
-                this.Cursor.Style = CursorStyle.Crosshair
-            }
-            if (event.Key == Key.Digit_5) {
-                this.Cursor.Style = CursorStyle.Progress
-            }
-            if (event.Key == Key.Digit_6) {
-                this.Cursor.Style = CursorStyle.Wait
-            }
-            if (event.Key == Key.Digit_7) {
-                this.Cursor.Style = CursorStyle.NotAllowed
-            }
-            if (event.Key == Key.Keypad_0) {
-                this.Cursor.Style = CursorStyle.Move
-            }
-            if (event.Key == Key.Keypad_1) {
-                this.Cursor.Style = CursorStyle.SingleArrowResize_N
-            }
-            if (event.Key == Key.Keypad_2) {
-                this.Cursor.Style = CursorStyle.SingleArrowResize_N_W
-            }
-            if (event.Key == Key.Keypad_3) {
-                this.Cursor.Style = CursorStyle.DoubleArrowResize_N_S
-            }
-            if (event.Key == Key.Keypad_4) {
-                this.Cursor.Style = CursorStyle.DoubleArrowResize_NW_SE
-            }
-        }
-
-        protected override fun OnKeyRepeat(event: KeyActionEvent) {
-        }
-
-        protected override fun OnKeyRelease(event: KeyActionEvent) {
-        }
-
-        protected override fun OnTextInput(text: String) {
-        }
-
-    }
 
     private val Engine: Engine
 
@@ -214,6 +90,128 @@ public class Program : AbstractProgram2<Theme, Screen, Router, Application> {
             }
         }
         return null
+    }
+
+}
+
+private class Engine2 : Engine {
+
+    public constructor() : super("Kotlin Game Example")
+
+    public override fun close() {
+        super.close()
+    }
+
+    protected override fun OnStart(info: FrameInfo) {
+    }
+
+    protected override fun OnStop(info: FrameInfo) {
+    }
+
+    protected override fun OnFixedUpdate(info: FrameInfo) {
+    }
+
+    protected override fun OnUpdate(info: FrameInfo) {
+    }
+
+}
+
+private class MainWindow2 : MainWindow {
+
+    public constructor() : super(MainWindowDesc.Window("Kotlin Game Example"))
+
+    public override fun close() {
+        super.close()
+    }
+
+    protected override fun OnShow() {
+    }
+
+    protected override fun OnHide() {
+    }
+
+    protected override fun OnDraw(info: FrameInfo) {
+    }
+
+    protected override fun OnMouseCursorMove(event: MouseCursorMoveEvent) {
+    }
+
+    protected override fun OnMouseButtonPress(event: MouseButtonActionEvent) {
+    }
+
+    protected override fun OnMouseButtonRelease(event: MouseButtonActionEvent) {
+    }
+
+    protected override fun OnMouseWheelScroll(event: MouseWheelScrollEvent) {
+    }
+
+    protected override fun OnKeyPress(event: KeyActionEvent) {
+        if (event.Key == Key.F1) {
+            this.IsFullScreen = !this.IsFullScreen
+        }
+
+        if (event.Key == Key.F2) {
+            this.IsResizable = !this.IsResizable
+        }
+
+        if (event.Key == Key.F3) {
+            this.Cursor.IsVisible = !this.Cursor.IsVisible
+        }
+        if (event.Key == Key.F4) {
+            this.Cursor.IsGrabbed = !this.Cursor.IsGrabbed
+        }
+        if (event.Key == Key.F5) {
+            this.Cursor.IsCaptured = !this.Cursor.IsCaptured
+        }
+        if (event.Key == Key.F6) {
+            this.Cursor.IsLocked = !this.Cursor.IsLocked
+        }
+
+        if (event.Key == Key.Digit_1) {
+            this.Cursor.Style = CursorStyle.Arrow
+        }
+        if (event.Key == Key.Digit_2) {
+            this.Cursor.Style = CursorStyle.Text
+        }
+        if (event.Key == Key.Digit_3) {
+            this.Cursor.Style = CursorStyle.Pointer
+        }
+        if (event.Key == Key.Digit_4) {
+            this.Cursor.Style = CursorStyle.Crosshair
+        }
+        if (event.Key == Key.Digit_5) {
+            this.Cursor.Style = CursorStyle.Progress
+        }
+        if (event.Key == Key.Digit_6) {
+            this.Cursor.Style = CursorStyle.Wait
+        }
+        if (event.Key == Key.Digit_7) {
+            this.Cursor.Style = CursorStyle.NotAllowed
+        }
+        if (event.Key == Key.Keypad_0) {
+            this.Cursor.Style = CursorStyle.Move
+        }
+        if (event.Key == Key.Keypad_1) {
+            this.Cursor.Style = CursorStyle.SingleArrowResize_N
+        }
+        if (event.Key == Key.Keypad_2) {
+            this.Cursor.Style = CursorStyle.SingleArrowResize_N_W
+        }
+        if (event.Key == Key.Keypad_3) {
+            this.Cursor.Style = CursorStyle.DoubleArrowResize_N_S
+        }
+        if (event.Key == Key.Keypad_4) {
+            this.Cursor.Style = CursorStyle.DoubleArrowResize_NW_SE
+        }
+    }
+
+    protected override fun OnKeyRepeat(event: KeyActionEvent) {
+    }
+
+    protected override fun OnKeyRelease(event: KeyActionEvent) {
+    }
+
+    protected override fun OnTextInput(text: String) {
     }
 
 }
