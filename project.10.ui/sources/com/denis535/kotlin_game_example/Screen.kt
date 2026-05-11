93d4e2fee76c3f4c69653d3f8cfa7722b73b099a
Denis <dddenisss35@gmail.com>
2026-05-11 23:16:51 +0300

    Update

--- project.10.ui/sources/com/denis535/kotlin_game_example/Screen.kt
+++ project.10.ui/sources/com/denis535/kotlin_game_example/Screen.kt
@@ -5,7 +5,7 @@ import com.denis535.game_framework_pro.*
 public class Screen : AbstractScreen2<Router, Application> {
 
     public constructor() {
-        this.Machine.SetRoot(RootWidget().Node, null, null)
+        this.Machine.SetRoot(RootWidget().BaseObject, null, null)
     }
 
     protected override fun OnClose() {
@@ -17,12 +17,12 @@ public class Screen : AbstractScreen2<Router, Application> {
 public class RootWidget : AbstractWidget2 {
 
     public constructor() {
-        this.NodeMutable.AddChild(MainWidget().Node, null)
-        this.NodeMutable.AddChild(GameWidget().Node, null)
+        this.BaseObject.AddChild(MainWidget().BaseObject, null)
+        this.BaseObject.AddChild(GameWidget().BaseObject, null)
     }
 
     protected override fun OnClose() {
-        this.NodeMutable.Children.asReversed().forEach { it.close() }
+        this.BaseObject.Children.asReversed().forEach { it.close() }
     }
 
     protected override fun OnActivate(argument: Any?) {
@@ -43,7 +43,7 @@ public class MainWidget : AbstractViewableWidget2 {
     }
 
     protected override fun OnClose() {
-        this.NodeMutable.Children.asReversed().forEach { it.close() }
+        this.BaseObject.Children.asReversed().forEach { it.close() }
     }
 
     protected override fun OnActivate(argument: Any?) {
@@ -64,7 +64,7 @@ public class GameWidget : AbstractViewableWidget2 {
     }
 
     protected override fun OnClose() {
-        this.NodeMutable.Children.asReversed().forEach { it.close() }
+        this.BaseObject.Children.asReversed().forEach { it.close() }
     }
 
     protected override fun OnActivate(argument: Any?) {
